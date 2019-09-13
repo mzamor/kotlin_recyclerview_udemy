91870d0caba85964de162584d7f33eb32acfd841
madaza <zamoramariano@hotmail.com>
2019-09-13 00:18:30 -0300

    formateo de codigo

--- app/src/main/java/com/example/recyclerviewkotlin/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewkotlin/MainActivity.kt
@@ -1,49 +1,50 @@
 package com.example.recyclerviewkotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ActionMode
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-import androidx.recyclerview.widget.DividerItemDecoration
-
 
 class MainActivity : AppCompatActivity() {
-    var isActionMode= false
-    var rvDishList:RecyclerView? = null
-    var adapter:AdapterCustom?= null
+    var isActionMode = false
+    var rvDishList: RecyclerView? = null
+    var adapter: AdapterCustom? = null
     var layoutManager: RecyclerView.LayoutManager? = null
     var swipeRefreshLayout: SwipeRefreshLayout? = null
-    var actionMode:ActionMode? = null
+    var actionMode: ActionMode? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val callBack = object: ActionMode.Callback{
+        val callBack = object : ActionMode.Callback {
             override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
 
-                when(item?.itemId){
+                when (item?.itemId) {
                     R.id.idelete -> adapter?.deleteDishes()
 
-                    else-> {return true}
+                    else -> {
+                        return true
+                    }
                 }
                 adapter?.finishActionMode()
                 mode?.finish()
-                isActionMode=false
+                isActionMode = false
                 return true
             }
 
             override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
                 adapter?.initActionMode()
                 actionMode = mode
-                isActionMode=true
-                menuInflater.inflate(R.menu.contextual_menu,menu!!)
+                isActionMode = true
+                menuInflater.inflate(R.menu.contextual_menu, menu!!)
                 return true
             }
 
@@ -54,62 +55,53 @@ class MainActivity : AppCompatActivity() {
 
             override fun onDestroyActionMode(p0: ActionMode?) {
                 adapter?.destroyActionMode()
-                isActionMode=false
+                isActionMode = false
             }
         }
 
-
-
         val dishes = ArrayList<Dish>()
-
-        dishes.add(Dish("Barbecue",300.00,4.00F,R.drawable.barbecue))
-        dishes.add(Dish("Empanadas",300.00,4.00F,R.drawable.empanadas))
-        dishes.add(Dish("Shawarma",300.00,4.00F,R.drawable.shawarma))
-        dishes.add(Dish("Spaguetties",300.00,4.00F,R.drawable.spaguetties))
-        dishes.add(Dish("Fish",300.00,4.00F,R.drawable.gish))
-
+        dishes.add(Dish("Barbecue", 300.00, 4.00F, R.drawable.barbecue))
+        dishes.add(Dish("Empanadas", 300.00, 4.00F, R.drawable.empanadas))
+        dishes.add(Dish("Shawarma", 300.00, 4.00F, R.drawable.shawarma))
+        dishes.add(Dish("Spaguetties", 300.00, 4.00F, R.drawable.spaguetties))
+        dishes.add(Dish("Fish", 300.00, 4.00F, R.drawable.gish))
         rvDishList = findViewById(R.id.rv_dish_list)
         rvDishList?.setHasFixedSize(true)
         rvDishList!!.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
 
-
         layoutManager = LinearLayoutManager(this)
         rvDishList?.layoutManager = layoutManager
-        adapter = AdapterCustom(this,dishes, object: ClickListener{
+        adapter = AdapterCustom(this, dishes, object : ClickListener {
             override fun onClick(view: View, position: Int) {
-                Toast.makeText(applicationContext,dishes.get(position).name,Toast.LENGTH_LONG).show()
-                if(isActionMode){
+                Toast.makeText(applicationContext, dishes.get(position).name, Toast.LENGTH_LONG)
+                    .show()
+                if (isActionMode) {
                     adapter?.selectDish(position)
                 }
-                actionMode?.title = adapter?.itemsSelected().toString() + " elementos seleccionados"
-
-                if(adapter?.itemsSelected()==0){
+                actionMode?.title = String.format(getString(R.string.items_selected),adapter?.itemsSelected().toString())
+                if (adapter?.itemsSelected() == 0)
                     actionMode?.finish()
-                }
 
             }
-
-        }, object:LongClickListener{
+        }, object : LongClickListener {
             override fun longClick(viev: View, position: Int) {
-                if(!isActionMode){
+                if (!isActionMode) {
                     startActionMode(callBack)
                     adapter?.selectDish(position)
-                    isActionMode=true
+                    isActionMode = true
                 }
-
-                if(adapter?.itemsSelected()==0){
+                actionMode?.title = String.format(getString(R.string.items_selected),adapter?.itemsSelected().toString())
+                if (adapter?.itemsSelected() == 0)
                     actionMode?.finish()
-                }
             }
         })
         rvDishList?.adapter = adapter
         swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout)
         swipeRefreshLayout?.setOnRefreshListener {
-            for(i in 1..10000){
-
+            for (i in 1..10000) {
             }
-            swipeRefreshLayout?.isRefreshing=false
-            dishes.add(Dish("Shawarma2",300.00,4.00F,R.drawable.shawarma2))
+            swipeRefreshLayout?.isRefreshing = false
+            dishes.add(Dish("Shawarma2", 300.00, 4.00F, R.drawable.shawarma2))
             adapter?.notifyDataSetChanged()
         }
     }
